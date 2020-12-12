41dd86652f10ac729add4f02e03b0e477037da61
MrUnderSan <MrUnderSan@gmail.com>
2020-12-12 20:39:16 +0300

    add carousel

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        sped: 1200,
+/*         adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+            
+        ]
+        
+
+    });
+  });
