6b1adb19ecff4b3294c084668691a0985cd290ee
MrUnderSan <MrUnderSan@gmail.com>
2020-12-13 18:55:26 +0300

    add tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         sped: 1200,
-/*         adaptiveHeight: true, */
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
         responsive: [
@@ -18,3 +17,21 @@ $(document).ready(function(){
 
     });
   });
+*/
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+});
+
+document.querySelector('.slick-prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.slick-next').addEventListener('click', function () {
+    slider.goTo('next');
+});
