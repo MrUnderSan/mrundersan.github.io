8a7394c86e758ec5d1103678d2a18c612258f560
MrUnderSan <MrUnderSan@gmail.com>
2021-01-14 20:06:57 +0300

    add validation

--- src/js/script.js
+++ src/js/script.js
@@ -71,4 +71,38 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+    
+            },
+            messages: {
+                name: {
+                    required: "We need your email address to contact you",
+                    minlength: jQuery.validator.format("At least {0} characters required!")
+                },
+                phone: "Please inter your phone",
+                email: {
+                  required: "We need your email address to contact you",
+                  email: "Your email address must be in the format of name@domain.com"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+375 (99) 999-99-99");
 });
